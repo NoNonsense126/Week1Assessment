b363aacf76ff0cdad30016553d4cbb748bf8cba8
Wong You Jing <wongyoujing118@gmail.com>
2016-01-15 09:17:41 -0800

    Initial Commit

---
+++ Week1Assessment - YouJing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Week1Assessment - YouJing
+//
+//  Created by Wong You Jing on 15/01/2016.
+//  Copyright © 2016 NoNonsense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
